68f1d45da6499a881c5fa97468300ad7ab3b1ab1
HernánPyCharm <carloshernangarrido@gmail.com>
2022-07-20 20:15:09 -0300

    first fully working version

--- const.py
+++ const.py
@@ -1,7 +1,5 @@
-import os
-
-
-SOURCE_PATH = "J:\\Fotos Hernan Garrido\\test"
+SOURCE_PATH = "J:\\Fotos Hernan Garrido"
 DESTINY_PATH = "J:\\organized"
+NONCOPIED_PATH = "J:\\nonorganized"
 YEAR_FIRST = 2014
 YEAR_LAST = 2022
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
+import shutil
+import datetime as dt
 import zipfile
 
 import PIL
 from PIL import Image
-import ffmpeg
+
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
 
 import const
+
+
+def creation_date(filename):
+    parser = createParser(filename)
+    metadata = extractMetadata(parser)
+    return metadata.get('creation_date')
 
 
 def mkdir_ifnotexists(path):
@@ -18,10 +28,10 @@ def mkdir_ifnotexists(path):
 
 def create_structure(path, year_first, year_last):
     mkdir_ifnotexists(path)
-    years = [str(y) for y in list(range(year_first, year_last+1))]
+    years = [str(y) for y in list(range(year_first, year_last + 1))]
     for year in years:
         mkdir_ifnotexists(os.path.join(path, year))
-        for month in range(1, 12+1):
+        for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
             mkdir_ifnotexists(os.path.join(path, year, str(month)))
     return years
 
@@ -31,31 +41,70 @@ def unzip_all(file_list):
         if zipfile.is_zipfile(file):
             print('unziping ', file, '...')
             dirname = os.path.splitext(file)[0]
-            try:
+            if not os.path.exists(dirname):
                 os.mkdir(dirname)
                 with zipfile.ZipFile(file, "r") as z:
                     z.extractall(dirname)
-            except FileExistsError:
+                print('File', file, 'correctly extracted')
+            else:
                 print('File', file, 'was already extracted')
 
 
-def organize_all(file_list):
-    for file in file_list:
+def get_year_month_day(file):
+    if os.path.splitext(file)[1] == '.zip' or os.path.splitext(file)[1] == '.json':
+        return False, None, None, None
+    try:
+        time_stamp = PIL.Image.open(file).getexif()[306]
+    except PIL.UnidentifiedImageError:
         try:
-            time_stamp = PIL.Image.open(file).getexif()[306]
-            print(file, time_stamp)
-        except PIL.UnidentifiedImageError:
-            print(file, 'is not an image')
-            # try:
-            p = ffmpeg.probe(file)
-            print(p)
-            # except
+            time_stamp = creation_date(file)
+        except ValueError:
+            time_stamp = None
+        except AttributeError:
+            time_stamp = None
+    except KeyError:
+        time_stamp = None
+    if time_stamp is not None:
+        if type(time_stamp) is str:
+            return True, time_stamp[0:4], time_stamp[5:7], time_stamp[8:10]
+        elif type(time_stamp) is dt.datetime:
+            return True, \
+                   str(time_stamp.year).zfill(4), str(time_stamp.month).zfill(2), str(time_stamp.day).zfill(2)
+    else:
+        return False, None, None, None
 
+
+def organize_all(file_list, dest):
+    noncopied_files = []
+    number_copied_files = 0
+    for file in file_list:
+        ismedia, year, month, day = get_year_month_day(file)
+        if ismedia:
+            print('copying ', file, ' to ', os.path.join(dest, year, month))
+            if not os.path.exists(os.path.join(dest, year)):
+                os.mkdir(os.path.join(dest, year))
+            if not os.path.exists(os.path.join(dest, year, month)):
+                os.mkdir(os.path.join(dest, year, month))
+            if not os.path.exists(os.path.join(dest, year, month, os.path.basename(file))):
+                shutil.copy(file, os.path.join(dest, year, month))
+                number_copied_files += 1
+        else:
+            if os.path.splitext(file)[1] == '.zip':
+                print('skiping', file)
+            else:
+                print('copying ', file, ' to ', const.NONCOPIED_PATH)
+                noncopied_files.append(file)
+                if not os.path.exists(const.NONCOPIED_PATH):
+                    os.mkdir(const.NONCOPIED_PATH)
+                shutil.copy(file, const.NONCOPIED_PATH)
+    return noncopied_files, number_copied_files
 
 
 if __name__ == '__main__':
-    print(create_structure(path=const.DESTINY_PATH, year_first=const.YEAR_FIRST, year_last=const.YEAR_LAST))
+    # years_created = create_structure(path=const.DESTINY_PATH, year_first=const.YEAR_FIRST, year_last=const.YEAR_LAST)
+    # print(years_created)
     files = [os.path.join(path, name) for path, subdirs, files in os.walk(const.SOURCE_PATH) for name in files]
     unzip_all(files)
-    organize_all(files)
+    not_copied, n_copied = organize_all(files, const.DESTINY_PATH)
+    print(f'*** {n_copied} copied files of {len(files)}')
     q = 0
