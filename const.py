3bf60773954c6c06da462b0d2c8296b648ab62f2
HernánPyCharm <carloshernangarrido@gmail.com>
2022-07-20 07:26:13 -0300

    initial commit

---
+++ const.py
@@ -0,0 +1,7 @@
+import os
+
+
+SOURCE_PATH = "J:\\Fotos Hernan Garrido"
+DESTINY_PATH = "J:\\organized"
+YEAR_FIRST = 2014
+YEAR_LAST = 2022
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import const
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def mkdir_ifnotexists(path):
+    """Create a directory and fail silently if exists"""
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
 
 
-# Press the green button in the gutter to run the script.
+def create_structure(path, year_first, year_last):
+    mkdir_ifnotexists(path)
+    years = [str(y) for y in list(range(year_first, year_last+1))]
+    for year in years:
+        mkdir_ifnotexists(os.path.join(path, year))
+        for month in range(1, 12+1):
+            mkdir_ifnotexists(os.path.join(path, year, str(month)))
+    return years
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(create_structure(path=const.DESTINY_PATH, year_first=const.YEAR_FIRST, year_last=const.YEAR_LAST))
+    a = [os.path.join(path, name) for path, subdirs, files in os.walk(const.SOURCE_PATH) for name in files]
+    q = 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # for ruta, directorios, archivos in os.walk(const.SOURCE_PATH, topdown=True):
+    #     print(ruta, directorios, archivos)
+        # print('\nruta       :', ruta)
+        # for elemento in archivos:
+        #     num_archivos += 1
+        #     archivo = ruta + os.sep + elemento
+        #     estado = os.stat(archivo)
+        #     tamaño = estado.st_size
+        #     ult_acceso = datetime.fromtimestamp(estado.st_atime)
+        #     modificado = datetime.fromtimestamp(estado.st_mtime)
+        #     ult_acceso = ult_acceso.strftime(formato)
+        #     modificado = modificado.strftime(formato)
+        #     total += tamaño
+        #     print(linea)
+        #     print('archivo      :', elemento)
+        #     print('modificado   :', modificado)
+        #     print('último acceso:', ult_acceso)
+        #     print('tamaño (Kb)  :', round(tamaño / 1024, 1))
